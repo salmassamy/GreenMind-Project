e528308dba87caadfc5535f1d23bade519ad8560
Rana-ELsofi <ranasofi987@gmail.com>
2025-12-17 17:48:48 +0200

    feat: Final updates and preparation for review on ArticlePage

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ArticleCard from '../components/ArticleCard';
-
 import articleImage from '../assets/Article Image.png'; 
 
 const ARTICLES_DATA = [
@@ -13,39 +12,34 @@ function ArticlePage() {
  return (
  <div className="article-page-layout">
 
-<h2 className="page-title">Reader's Choice</h2>
-<p className="page-subtitle">This month's most popular article is: "Why AI is the Best Tool for Predicting Wheat's Nitrogen Needs."</p>
+    <h2 className="readers-choice-title">Reader's Choice</h2>
+    <p className="readers-choice-subtitle">This month's most popular article is: "Why AI is the Best Tool for Predicting Wheat's Nitrogen Needs."</p>
 
- <div className="featured-today-box"> 
-            
-            <h3 className="featured-main-title">FEATURED TODAY</h3>
-            
- <div className="featured-image-wrapper">
-                <img src={articleImage} alt="Featured Article" className="featured-image" />
-</div>
-            
-            <div className="featured-bottom-content">
-                <p>Learn easy ways to protect soil, save water, and grow sustainably.</p>
-                <a href="#" className="read-more-featured">Read more</a>
-            </div>
+    <div className="featured-today-box"> 
+        <h3 className="featured-main-title">FEATURED TODAY</h3>
+        <div className="featured-image-wrapper">
+            <img src={articleImage} alt="Featured Article" className="featured-image" />
+        </div>
+        <div className="featured-bottom-content">
+            <p>Learn easy ways to protect soil, save water, and grow sustainably.</p>
+            <a href="#" className="read-more-featured">Read more</a>
+        </div>
+    </div>
 
- </div>
+    <h2 className="readers-choice-title">Article</h2>
+    <p className="readers-choice-subtitle">This month's most popular article is: "Why AI is the Best Tool for Predicting Wheat's Nitrogen Needs."</p>
 
- <h2 className="page-title article-section-title">Article</h2>
- <p className="page-subtitle article-section-subtitle">This month's most popular article is: "Why AI is the Best Tool for Predicting Wheat's Nitrogen Needs."</p>
-
-
- <div className="articles-grid">
- {ARTICLES_DATA.map(article => (
-<ArticleCard 
- key={article.id}
-title={article.title}
- description={article.description}
- link={article.link}
-imageUrl={article.imageUrl} 
- />
- ))}
- </div>
+    <div className="articles-grid">
+        {ARTICLES_DATA.map(article => (
+            <ArticleCard 
+                key={article.id}
+                title={article.title}
+                description={article.description}
+                link={article.link}
+                imageUrl={article.imageUrl} 
+            />
+        ))}
+    </div>
 
  </div>
  );
